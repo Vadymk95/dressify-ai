8edb3522b7fa24e03199898115af2597ae99ecf7
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2025-03-19 15:12:07 +0200

    add a basic router

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,5 +1,14 @@
 import { FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { Home } from '@root/pages/Home';
+
+import { routes } from './routes';
 
 export const AppRouter: FC = () => {
-    return <div></div>;
+    return (
+        <Routes>
+            <Route path={routes.home} element={<Home />} />
+        </Routes>
+    );
 };
