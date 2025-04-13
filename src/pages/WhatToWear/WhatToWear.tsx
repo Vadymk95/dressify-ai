6ee5bbde227bfa6bf15f7f802cae70053c825202
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2025-04-13 21:19:07 +0300

    feat(PanelsContainer): decompose panels plus refactor ui store

--- src/pages/WhatToWear/WhatToWear.tsx
+++ src/pages/WhatToWear/WhatToWear.tsx
@@ -1,24 +1,14 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { EmailVerificationPanel } from '@/components/common/EmailVerificationPanel';
+import { PanelContainer } from '@/components/common/PanelContainer';
 import { PerfectOutlook } from '@/components/common/PerfectOutlook';
-import { EventPanel } from '@/components/features/EventPanel';
 import { Feedback } from '@/components/features/Feedback';
 import { OutfitRequestPanel } from '@/components/features/OutfitRequestPanel';
-import { PersonalDetailsPanel } from '@/components/features/PersonalDetailsPanel';
-import { WardrobePanel } from '@/components/features/WardrobePanel';
-import { WeatherPanel } from '@/components/features/WeatherPanel';
-import { Switch } from '@/components/ui/switch';
 
 const WhatToWear: FC = () => {
     const { t } = useTranslation();
-    const [isMinimalistic, setIsMinimalistic] = useState(false);
-    const isMinimalisticView = isMinimalistic ? 'h-auto' : 'h-[200px]';
-
-    const handleSwitchChange = () => {
-        setIsMinimalistic(!isMinimalistic);
-    };
 
     return (
         <div className="w-full max-w-4xl mx-auto px-6 flex flex-col gap-10">
@@ -33,36 +23,7 @@ const WhatToWear: FC = () => {
                 </p>
             </div>
 
-            <section className="flex flex-col gap-2 w-full mx-auto">
-                <div className="flex justify-end items-center gap-2">
-                    <span className="text-amber-700 text-sm">
-                        {isMinimalistic
-                            ? t('Pages.WhatToWear.minimalisticView')
-                            : t('Pages.WhatToWear.detailedView')}
-                    </span>
-                    <Switch
-                        className="data-[state=checked]:bg-amber-500 cursor-pointer"
-                        checked={isMinimalistic}
-                        onCheckedChange={handleSwitchChange}
-                    />
-                </div>
-                <div
-                    className={`w-full grid gap-2 ${isMinimalistic ? 'grid-cols-4' : 'grid-cols-1 md:grid-cols-2'}`}
-                >
-                    <div className={isMinimalisticView}>
-                        <WeatherPanel isMinimalistic={isMinimalistic} />
-                    </div>
-                    <div className={isMinimalisticView}>
-                        <PersonalDetailsPanel isMinimalistic={isMinimalistic} />
-                    </div>
-                    <div className={isMinimalisticView}>
-                        <WardrobePanel isMinimalistic={isMinimalistic} />
-                    </div>
-                    <div className={isMinimalisticView}>
-                        <EventPanel isMinimalistic={isMinimalistic} />
-                    </div>
-                </div>
-            </section>
+            <PanelContainer />
 
             <section className="p-4 bg-amber-50 rounded-xl shadow-md text-center">
                 <OutfitRequestPanel />
