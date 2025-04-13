f4a3dff68eea59bba91b68561ae17a18767950b0
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2025-04-13 21:30:39 +0300

    fix(Loader): remove scroll when loading

--- src/components/common/Loader/index.tsx
+++ src/components/common/Loader/index.tsx
@@ -1,19 +1,35 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 export const Loader: FC = () => {
+    useEffect(() => {
+        document.body.classList.add('overflow-hidden');
+        document.documentElement.classList.add('overflow-hidden');
+        document.body.classList.add('h-screen');
+        document.body.classList.add('w-screen');
+
+        return () => {
+            document.body.classList.remove('overflow-hidden');
+            document.documentElement.classList.remove('overflow-hidden');
+            document.body.classList.remove('h-screen');
+            document.body.classList.remove('w-screen');
+        };
+    }, []);
+
     return (
         <div
             className="
             fixed
             inset-0
-            z-50
+            z-[9999]
             flex
             items-center
             justify-center
             bg-black/20
             bg-opacity-50
             backdrop-blur-sm
+            overflow-hidden
           "
+            style={{ touchAction: 'none' }}
         >
             <div
                 className="
